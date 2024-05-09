0d5c55c8a6dfa1624ae08e36704908e2a4dceb21
trungnguyen29775 <ITITIU20332@student.hcmiu.edu.vn>
2024-05-09 14:30:53 +0700

    9/5/2024: Done main

--- front-end/src/components/main/main.js
+++ front-end/src/components/main/main.js
@@ -6,6 +6,7 @@ import { MdHelp } from 'react-icons/md';
 import { FiSearch } from 'react-icons/fi';
 import { IoMdClose } from 'react-icons/io';
 import { useEffect, useState } from 'react';
+import { RiMore2Fill } from 'react-icons/ri';
 
 const Main = function () {
     const [searchInput, setSearchInput] = useState('');
@@ -30,17 +31,20 @@ const Main = function () {
                     <span className="nav-bar-item color--white background-color--green">Add Friend</span>
                 </div>
                 <div className="nav-bar-item-container">
-                    <div className="nav-bar-item">
-                        <IoChatbubbleEllipsesSharp style={{ fontSize: '25px' }} />
+                    <div className="nav-bar-item add-label">
+                        <IoChatbubbleEllipsesSharp style={{ fontSize: '25px' }} aria-label="New group DM" />
+                        <span className="hover-label">New group DM</span>
                     </div>
                     <div className="seperate-bar--straight"></div>
 
-                    <div className="nav-bar-item last">
+                    <div className="nav-bar-item last add-label">
                         <BiSolidInbox style={{ fontSize: '25px' }} />
+                        <span className="hover-label">Inbox</span>
                     </div>
 
-                    <div className="nav-bar-item last">
+                    <div className="nav-bar-item last add-label">
                         <MdHelp style={{ fontSize: '25px' }} />
+                        <span className="hover-label">Help</span>
                     </div>
                 </div>
             </div>
@@ -58,18 +62,48 @@ const Main = function () {
                         <IoMdClose className="rotate" style={{ fontSize: '20px' }} />
                     )}
                 </div>
-                <div className="main-home-content-item-container">
-                    <span className="main-home-content-item-container__header">ONLINE - 1</span>
-                    <div className="home-control-bar-item">
-                        <div className="seperate-bar"></div>
-                        <div className="avt-user-container">
-                            <img src="/image/cat.jpg" className="avt-user" />
+                <span className="main-home-content-item-container__header">ONLINE - 1</span>
+
+                {[1, 2, 3, 4].map((data, index) => {
+                    return (
+                        <div key={index} className="main-home-content-item-container">
+                            <div className="home-control-bar-item main">
+                                <div className="flex--row">
+                                    <div className="avt-user-container">
+                                        <img src="/image/cat.jpg" className="avt-user" />
+                                    </div>
+                                    <div className="name-status-user-container">
+                                        <span>Stupid Cat</span>
+                                        <span className="status">Offline</span>
+                                    </div>
+                                </div>
+                                <div className="flex--row">
+                                    <div className="icon-container--circle add-label">
+                                        <IoChatbubbleEllipsesSharp style={{ fontSize: '20px', margin: 'auto' }} />
+                                        <span className="hover-label">Message</span>
+                                    </div>
+                                    <div className="icon-container--circle add-label">
+                                        <RiMore2Fill style={{ fontSize: '20px', margin: 'auto' }} />
+                                        <span className="hover-label">More</span>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <span>Stupid Cat</span>
+                    );
+                })}
+            </div>
+            <div className="main-right-bar home">
+                <span className="main-right-bar-home__header">Activity Now</span>
+                <div className="main-right-bar-home-container">
+                    <div className="main-right-bar-home-container-notify-container">
+                        <span className="right-bar-home__subtitle">It's quite for now...</span>
+                        <span className="right-bar-home__subtitle--smaller">
+                            When a friend starts an activity—like playing a game or hanging out on voice—we’ll show it
+                            here!
+                        </span>
                     </div>
                 </div>
             </div>
-            <div className="main-right-bar"></div>
         </div>
     );
 };
