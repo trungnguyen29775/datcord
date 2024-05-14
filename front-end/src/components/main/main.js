af41ee8de90d838deea2831613badb508571fb9f
trungnguyen29775 <ITITIU20332@student.hcmiu.edu.vn>
2024-05-14 15:16:56 +0700

    14/5/2024: 90% Done home page

--- front-end/src/components/main/main.js
+++ front-end/src/components/main/main.js
@@ -7,6 +7,7 @@ import { FiSearch } from 'react-icons/fi';
 import { IoMdClose } from 'react-icons/io';
 import { useEffect, useState } from 'react';
 import { RiMore2Fill } from 'react-icons/ri';
+import NoContentImage from '../../constant/noContentImage';
 
 const Main = function () {
     const [searchInput, setSearchInput] = useState('');
@@ -14,6 +15,13 @@ const Main = function () {
 
     const handleChangeSearchInput = (event) => {
         setSearchInput(event.target.value);
+    };
+
+    const handleChangeNavFriend = (event) => {
+        const currentActive = document.querySelector('.nav-bar-item.active');
+        currentActive.classList.remove('active');
+        event.target.classList.add('active');
+        setHomeNavStatus(event.target.textContent.toLowerCase());
     };
 
     return (
@@ -25,11 +33,24 @@ const Main = function () {
                         <span>Friends</span>
                     </div>
                     <div className="seperate-bar--straight"></div>
-                    <span className="nav-bar-item active">Online</span>
-                    <span className="nav-bar-item">All</span>
-                    <span className="nav-bar-item">Pending</span>
-                    <span className="nav-bar-item">Blocked</span>
-                    <span className="nav-bar-item color--white background-color--green">Add Friend</span>
+                    <span className="nav-bar-item active" onClick={(e) => handleChangeNavFriend(e)}>
+                        Online
+                    </span>
+                    <span className="nav-bar-item" onClick={(e) => handleChangeNavFriend(e)}>
+                        All
+                    </span>
+                    <span className="nav-bar-item" onClick={(e) => handleChangeNavFriend(e)}>
+                        Pending
+                    </span>
+                    <span className="nav-bar-item" onClick={(e) => handleChangeNavFriend(e)}>
+                        Blocked
+                    </span>
+                    <span
+                        className="nav-bar-item color--white background-color--green"
+                        onClick={(e) => handleChangeNavFriend(e)}
+                    >
+                        Add Friend
+                    </span>
                 </div>
                 <div className="nav-bar-item-container">
                     <div className="nav-bar-item add-label">
@@ -49,49 +70,148 @@ const Main = function () {
                     </div>
                 </div>
             </div>
-            <div className="main-content-container">
-                <div className="main-home-content-header">
-                    <input
-                        onChange={(e) => handleChangeSearchInput(e)}
-                        className="main-home-content-header__input"
-                        placeholder="Search"
-                        value={searchInput}
-                    />
-                    {searchInput?.length <= 0 ? (
-                        <FiSearch className="rotate" style={{ fontSize: '20px' }} />
-                    ) : (
-                        <IoMdClose className="rotate" style={{ fontSize: '20px' }} />
-                    )}
-                </div>
-                <span className="main-home-content-item-container__header">ONLINE - 1</span>
 
-                {[1, 2, 3, 4].map((data, index) => {
-                    return (
-                        <div key={index} className="main-home-content-item-container">
-                            <div className="home-control-bar-item main">
-                                <div className="flex--row">
-                                    <div className="avt-user-container">
-                                        <img src="/image/cat.jpg" className="avt-user" />
+            {/* Main Content */}
+            <div className="main-content-container">
+                {homeFriendNavStatus != 'add friend' ? (
+                    <div className="main-home-content-header">
+                        <input
+                            onChange={(e) => handleChangeSearchInput(e)}
+                            className="main-home-content-header__input"
+                            placeholder="Search"
+                            value={searchInput}
+                        />
+                        {searchInput?.length <= 0 ? (
+                            <FiSearch className="rotate" style={{ fontSize: '20px' }} />
+                        ) : (
+                            <IoMdClose className="rotate" style={{ fontSize: '20px' }} />
+                        )}
+                    </div>
+                ) : (
+                    ''
+                )}
+
+                {/* Content Home */}
+                <span className="main-home-content-item-container__header">
+                    {homeFriendNavStatus === 'online' ? 'Online -1' : ''}
+                </span>
+                {homeFriendNavStatus === 'online' ? (
+                    [1, 2, 3, 4].map((data, index) => {
+                        return (
+                            // Online friend
+
+                            <div key={index} className="main-home-content-item-container">
+                                <div className="home-control-bar-item main">
+                                    <div className="flex--row">
+                                        <div className="avt-user-container">
+                                            <img src="/image/cat.jpg" className="avt-user" />
+                                        </div>
+                                        <div className="name-status-user-container">
+                                            <span>Stupid Cat</span>
+                                            <span className="status">Offline</span>
+                                        </div>
                                     </div>
-                                    <div className="name-status-user-container">
-                                        <span>Stupid Cat</span>
-                                        <span className="status">Offline</span>
-                                    </div>
-                                </div>
-                                <div className="flex--row">
-                                    <div className="icon-container--circle add-label">
-                                        <IoChatbubbleEllipsesSharp style={{ fontSize: '20px', margin: 'auto' }} />
-                                        <span className="hover-label">Message</span>
-                                    </div>
-                                    <div className="icon-container--circle add-label">
-                                        <RiMore2Fill style={{ fontSize: '20px', margin: 'auto' }} />
-                                        <span className="hover-label">More</span>
+                                    <div className="flex--row">
+                                        <div className="icon-container--circle add-label">
+                                            <IoChatbubbleEllipsesSharp style={{ fontSize: '20px', margin: 'auto' }} />
+                                            <span className="hover-label">Message</span>
+                                        </div>
+                                        <div className="icon-container--circle add-label">
+                                            <RiMore2Fill style={{ fontSize: '20px', margin: 'auto' }} />
+                                            <span className="hover-label">More</span>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                        );
+                    })
+                ) : homeFriendNavStatus === 'all' ? (
+                    [1, 2, 1, 3, 5, 1, 2].map((data, index) => {
+                        return (
+                            // All friend
+                            <div key={index} className="main-home-content-item-container">
+                                <div className="home-control-bar-item main">
+                                    <div className="flex--row">
+                                        <div className="avt-user-container">
+                                            <img src="/image/cat.jpg" className="avt-user" />
+                                        </div>
+                                        <div className="name-status-user-container">
+                                            <span>Stupid Cat</span>
+                                            <span className="status">Offline</span>
+                                        </div>
+                                    </div>
+                                    <div className="flex--row">
+                                        <div className="icon-container--circle add-label">
+                                            <IoChatbubbleEllipsesSharp style={{ fontSize: '20px', margin: 'auto' }} />
+                                            <span className="hover-label">Message</span>
+                                        </div>
+                                        <div className="icon-container--circle add-label">
+                                            <RiMore2Fill style={{ fontSize: '20px', margin: 'auto' }} />
+                                            <span className="hover-label">More</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })
+                ) : homeFriendNavStatus === 'pending' ? (
+                    [1, 2, 3, 4, 5].map((data, index) => {
+                        return (
+                            // Pending friend request
+                            <div key={index} className="main-home-content-item-container">
+                                <div className="home-control-bar-item main">
+                                    <div className="flex--row">
+                                        <div className="avt-user-container">
+                                            <img src="/image/cat.jpg" className="avt-user" />
+                                        </div>
+                                        <div className="name-status-user-container">
+                                            <span>Stupid Cat</span>
+                                            <span className="status">Offline</span>
+                                        </div>
+                                    </div>
+                                    <div className="flex--row">
+                                        <div className="icon-container--circle add-label">
+                                            <IoChatbubbleEllipsesSharp style={{ fontSize: '20px', margin: 'auto' }} />
+                                            <span className="hover-label">Message</span>
+                                        </div>
+                                        <div className="icon-container--circle add-label">
+                                            <RiMore2Fill style={{ fontSize: '20px', margin: 'auto' }} />
+                                            <span className="hover-label">More</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })
+                ) : homeFriendNavStatus === 'blocked' ? (
+                    [1].map((data, index) => {
+                        return (
+                            // Blocked list
+                            <div key={index} className="main-home-content-item-container">
+                                <NoContentImage />
+                            </div>
+                        );
+                    })
+                ) : (
+                    // Add friend
+                    <div className="main-home-content-item-container">
+                        <div className="home-add-friend-header">
+                            <h3>ADD FRIEND</h3>
+                            <span>You can add friends with their Discord username</span>
+                            <div className="home-add-friend-header__search">
+                                <input
+                                    placeholder="You can add friends with their Discord username"
+                                    className="home-add-friend__input"
+                                />
+                                <button>Send Friend Request</button>
+                            </div>
                         </div>
-                    );
-                })}
+                        <div className="seperate-bar--horizontal"></div>
+                        <div className="no-content-container">
+                            <NoContentImage />
+                        </div>
+                    </div>
+                )}
             </div>
             <div className="main-right-bar home">
                 <span className="main-right-bar-home__header">Activity Now</span>
