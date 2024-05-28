c242e0b6f47fcd718723be75ec0f5ed760874fb1
trungnguyen29775 <91877086+npdqba02@users.noreply.github.com>
2024-05-28 11:30:49 +0700

    commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,7 +3,7 @@ import LogIn from './page/login/login';
 import Register from './page/register/register';
 
 function App() {
-    return <div></div>;
+    return <Register />;
 }
 
 export default App;
