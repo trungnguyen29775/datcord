ba79f4a7b3a17411b5aa52f62270467d4283c3d7
trungnguyen29775 <91877086+npdqba02@users.noreply.github.com>
2024-06-01 13:49:22 +0700

    1/6/2024: Register

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,7 +3,7 @@ import LogIn from './page/login/login';
 import Register from './page/register/register';
 
 function App() {
-    return <MainLayout />;
+    return <Register />;
 }
 
 export default App;
