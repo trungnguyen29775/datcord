426c6e62a1243f18b6beea3389349e74a1b38b34
trungnguyen29775 <91877086+npdqba02@users.noreply.github.com>
2024-07-15 14:01:50 +0700

    Check

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -61,4 +61,6 @@ io.on('connection', (socket) => {
 require('./controller/user.controller')(app);
 require('./controller/friend.controller')(app);
 
-httpServer.listen(port, () => {});
+httpServer.listen(port, () => {
+    console.log('Listen on port ', port);
+});
